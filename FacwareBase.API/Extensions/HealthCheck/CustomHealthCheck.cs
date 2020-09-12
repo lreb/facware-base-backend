9472ac8af66ce13febe30f33639352fc1d33c6f2
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2020-09-12 09:22:06 -0600

    add version 1 - basic stuffs to start to ork with api
    swagger, several environments, healthcheck, dbcontext, extensions, cors, appsetings service, aws lambda endpoint configuration

---
+++ FacwareBase.API/Extensions/HealthCheck/CustomHealthCheck.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FacwareBase.Api.Extensions.HealthCheck
+{
+	/// <summary>
+	/// Custom health check
+	/// </summary>
+	public class CustomHealthCheckExtension: IHealthCheck
+	{
+		/// <summary>
+		/// App settings data DI
+		/// </summary>
+		private readonly IConfiguration _config;
+
+		public CustomHealthCheckExtension(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		/// <summary>
+		/// custom health check 
+		/// </summary>
+		/// <param name="context">Health check context <see cref="HealthCheckContext"/></param>
+		/// <param name="cancellationToken">cancellation token <see cref="CancellationToken"/></param>
+		/// <returns></returns>
+		public Task<HealthCheckResult> CheckHealthAsync(
+			HealthCheckContext context,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			Dictionary<string, object> data = new Dictionary<string, object>();
+			data["environment"] = _config["HealthChecks:Environment"];
+			data["corsAllowedOrigin"] = _config.GetSection("Cors:AllowedOrigin").Get<string[]>();
+			// data["connection"] = _config["ConnectionStrings:ApplicationConfigurationConnectionString"];
+			return Task.FromResult(
+				HealthCheckResult.Healthy("Test health check data", data));
+		}
+    }
+}
---
+++ FacwareBase.API/Helpers/Domain/ConectionString.cs
@@ -0,0 +1,7 @@
+namespace FacwareBase.API.Helpers.Domain
+{
+  public class ConnectionString
+  {
+    public string ApplicationConfigurationConnectionString { get; set; }    
+  }    
+}
