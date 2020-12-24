ff11ca7c276d5092dd9c62af8dbff14c76833766
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2020-12-24 16:24:53 -0700

    add net 5 projects

---
+++ Facware.Data.Access/Readme.md
@@ -0,0 +1,5 @@
+dotnet add package Microsoft.EntityFrameworkCore.Tools.DotNet --version 2.0.3
+
+dotnet ef migrations add init --startup-project ../Facware.Api --verbose
+
+dotnet ef migrations update --startup-project ../Facware.Api --verbose
---
+++ Facware.Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Facware.Models
+{
+    public class Item
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemId { get; set; }
+        [Column(TypeName = "varchar"), MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
