d3def73d02f4e698c427dbe790d72cb23f58b9de
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2020-12-29 18:39:07 -0700

    add extensions
    include cors, database, swagger and environment extensions. remove extension project

---
+++ Facware.Api/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Facware.Api.Extensions
+{
+    public class DependencyInjectionExtension
+    {
+        public DependencyInjectionExtension()
+        {
+        }
+    }
+}
--- Facware.Api/Startup.cs
+++ Facware.Api/Startup.cs
@@ -1,20 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Facware.Data.Access;
+using Facware.Api.Extensions;
 using Facware.Data.Access.Base.Base;
 using Facware.Data.Access.Repository.Implementation;
 using Facware.Data.Access.Repository.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Facware.Api
@@ -31,18 +23,36 @@ namespace Facware.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Swagger service
+            // enable swagger service
+            services.ConfigureSwaggerExtension(_configuration);
+            #endregion
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            /*services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Facware.Api", Version = "v1" });
-            });
+            });*/
 
-            var connectionString = _configuration["DbContextSettings:ConnectionString"];
-            services.AddDbContext<FacwareDbContext>(options =>
+            //var connectionString = _configuration["DbContextSettings:ConnectionString"];
+            /* services.AddDbContext<FacwareDbContext>(options =>
                 options.UseNpgsql(
                     connectionString,
-                    x => x.MigrationsAssembly("Facware.Data.Access")));
+                    x => x.MigrationsAssembly("Facware.Data.Access"))); */
+
+
+            #region Database context service
+            // TODO: before use this, create your own dbcontext
+            services.UsePostgreSqlServer(_configuration);
+
+            // in memory db
+            // services.UseInMemoryDatabase();
+            #endregion
+
+            #region Cors service
+            // enable policy cors service
+            services.ConfigureCors(_configuration);
+            #endregion
 
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddTransient<IItemRepository, ItemRepository>();
@@ -52,11 +62,26 @@ namespace Facware.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            // Cors pipe
+            app.UseCors(CorsExtension.AllowSpecificOrigins);
+            // handle several environments
+            if (env.IsLocal())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Facware.Api v1"));
+                app.EnableSwaggerPipeline(_configuration);
+            }
+            else if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.EnableSwaggerPipeline(_configuration);
+            }
+            else if (env.IsStaging())
+            {
+                app.EnableSwaggerPipeline(_configuration);
+            }
+            else
+            {
+                app.EnableSwaggerPipeline(_configuration);
             }
 
             app.UseHttpsRedirection();
