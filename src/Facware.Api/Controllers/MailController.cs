18d7e4dcc9eafdcc45e537c90af38ad7799af0b9
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2021-01-16 09:42:00 -0700

    add extensions and api layer

---
+++ src/Facware.Api/Controllers/MailController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Facware.Domain.Settings;
+using Facware.Service.Contract;
+using System.Threading.Tasks;
+
+namespace Facware.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/Mail")]
+    [ApiVersion("1.0")]
+    public class MailController : ControllerBase
+    {
+        private readonly IEmailService mailService;
+        public MailController(IEmailService mailService)
+        {
+            this.mailService = mailService;
+        }
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequest request)
+        {
+            await mailService.SendEmailAsync(request);
+            return Ok();
+        }
+
+    }
+}
---
+++ src/Facware.Infraestructure/Mapping/CustomerProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Facware.Domain.Entities;
+using Facware.Infrastructure.ViewModel;
+
+namespace Facware.Infrastructure.Mapping
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<CustomerModel, Customer>()
+                .ForMember(dest => dest.Id,
+                        opt => opt.MapFrom(src => src.CustomerId))
+                .ReverseMap();
+        }
+    }
+}
