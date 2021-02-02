547c7aa96f1dd4e5180e84d9c14ce63f2556c9b9
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2021-02-01 19:53:18 -0700

    updated generic extensions

---
+++ src/Facware.Infraestructure/Extension/OData/ODataExtension.cs
@@ -0,0 +1,44 @@
+// using System;
+// using FacwareBase.API.Helpers.Domain.POCO;
+// using Microsoft.AspNet.OData.Builder;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.OData.Edm;
+
+// namespace FacwareBase.Api.Extensions.OData
+// {
+// 	/// <summary>
+// 	/// OData extension
+// 	/// </summary>
+// 	public static class ODataExtension
+// 	{
+// 		/// <summary>
+// 		/// Generates the OData IEdm Models based on the DB Tables/Models
+// 		/// </summary>
+// 		/// <param name="serviceProvider"></param>
+// 		/// <returns><see cref="IEdmModel"/></returns>
+// 		public static IEdmModel GetODataModels(IServiceProvider serviceProvider)
+// 		{
+// 			ODataModelBuilder builder = new ODataConventionModelBuilder(serviceProvider).EnableLowerCamelCase();
+
+// 			builder.EntitySet<Song>("Songs").EntityType.Filter();
+// 			builder.EntitySet<Album>("Albums");
+
+// 			return builder.GetEdmModel();
+// 		}
+
+// 		/// <summary>
+// 		/// Generates the OData IEdm Models based on the DB Tables/Models extension method
+// 		/// </summary>
+// 		/// <param name="app"></param>
+// 		/// <returns><see cref="IEdmModel"/></returns>
+// 		public static IEdmModel GetODataModels(this IApplicationBuilder app)
+// 		{
+// 			ODataModelBuilder builder = new ODataConventionModelBuilder(app.ApplicationServices).EnableLowerCamelCase();
+
+// 			builder.EntitySet<Song>("Songs");
+// 			builder.EntitySet<Album>("Album");
+
+// 			return builder.GetEdmModel();
+// 		}
+// 	}
+// }
